73ad74dff6c328c9dd610c78c51966ea05b3d894
= <=>
2024-03-09 03:47:40 +0100

    segunda version - decimos lo que saca la maquina

--- piedrapapelotijera.py
+++ piedrapapelotijera.py
@@ -10,3 +10,10 @@ tiradaordenador = random.randint(1,3)
 
 print(tirada)
 print(tiradaordenador)
+
+if tiradaordenador==1:
+	print("el ordenador ha sacado piedra")
+elif tiradaordenador==2;
+	print("el ordenador ha sacado papel")
+elif tiradaordenador==3;
+	print("el ordenador ha sacado tijera")

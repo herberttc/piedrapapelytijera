abde670e94593821a92cb6f9bc25430354f0e33b
= <=>
2024-03-09 03:43:16 +0100

    mi primera version de mi juego

---
+++ piedrapapelotijera.py
@@ -0,0 +1,12 @@
+import random
+
+print("1=piedra")
+print("1=papel")
+print("1=tijera")
+
+tirada = input("escoge tu tirada")
+
+tiradaordenador = random.randint(1,3)
+
+print(tirada)
+print(tiradaordenador)
